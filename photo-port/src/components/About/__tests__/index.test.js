a12e8184a3fc228ef73e0f73338bffb453b33773
hthomp15 <hailethomp@gmail.com>
2022-05-12 16:37:01 -0400

    add tests for nav and about components

--- photo-port/src/components/About/__tests__/index.test.js
+++ photo-port/src/components/About/__tests__/index.test.js
@@ -1,20 +1,19 @@
+// __tests__/About.test.js
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
 
 afterEach(cleanup);
-describe('About component', () => {
-    // Renders about text
-    // First Test: 
-    it('renders about text', () => {
-        render(<About />);
-    })
-    // Second Test:
-    it('matches snapshot DOM node sturcture', () => {
-        //renders about
-        const { asFragment } = render(<About />);
-        expect(asFragment()).toMatchSnapshot();
-    })
 
+describe('About component', () => {
+  it('renders', () => {
+    render(<About />);
+  });
+  
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<About />);
+    
+    expect(asFragment()).toMatchSnapshot();
+  });
 })
