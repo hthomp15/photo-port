db79693cd3f1b66b2794e8e8393d0f086602986e
hthomp15 <hailethomp@gmail.com>
2022-05-14 11:26:36 -0400

    add photo gallery as child component, update tests, raise state of commercial props to app.js

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -1,16 +1,32 @@
-import './App.css';
-import About from './components/About/index.js';
-import Nav from './components/Nav/index.js';
-import Gallery from './components/Gallery/index.js';
+import React, { useState } from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+import Gallery from './components/Gallery';
 
 function App() {
+  const [categories] = useState([
+    {
+      name: 'commercial',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    },
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+  ]); 
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
-    <div className="App">
-        <Nav></Nav>
+    <div>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Nav>
       <main>
-        <Gallery></Gallery>
+        <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
-      </main>  
+      </main>
     </div>
   );
 }
