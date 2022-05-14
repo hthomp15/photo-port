9b03145929dd8f5495180c3d049f3f1a5ec58278
hthomp15 <hailethomp@gmail.com>
2022-05-13 23:47:11 -0400

    add photos in Gallery/index.js

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import About from './components/About/index.js';
 import Nav from './components/Nav/index.js';
+import Gallery from './components/Gallery/index.js';
 
 function App() {
   return (
     <div className="App">
         <Nav></Nav>
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>  
     </div>
