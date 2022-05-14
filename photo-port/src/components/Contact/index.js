2d13e059f10169faa61b65d42e6c9fe30ae62315
hthomp15 <hailethomp@gmail.com>
2022-05-14 12:20:35 -0400

    add contact form and handler functions

--- photo-port/src/components/Contact/index.js
+++ photo-port/src/components/Contact/index.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ContactForm(){
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+    const handleChange = e => {
+        setFormState({ ...formState, [e.target.name]: e.target.value });
+    };
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log('Form submitted');
+        console.log(formState);
+    };
+
     return (
         <section>
             <h1>Contact Form</h1>
-            <form id="contact-form">
+            <form id="contact-form" onSubmit={handleSubmit}>
                 <label htmlFor="name">Name</label>
-                <input type="text" name="name" />
+                <input type="text" name="name" onChange={handleChange} defaultValue={name}/>
                 <label htmlFor="email">Email</label>
-                <input type="email" name="email" />
+                <input type="email" name="email" onChange={handleChange} defaultValue={email} />
                 <label htmlFor="message">Message</label>
-                <textarea name="message"></textarea>
+                <textarea name="message" onChange={handleChange} defaultValue={message}></textarea>
                 <button type="submit">Submit</button>
             </form>
         </section>
